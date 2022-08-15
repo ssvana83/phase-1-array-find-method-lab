f17f68cd252f9cb1ba428c5a99985818f5d38e05
stacyvana <ssvana83@yahoo.com>
2022-08-15 13:36:16 -0400

    commit tree 501c16862aa721f1aa7b7513473df0c25f48d7a4

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"}
+]
+
+function superbowlWin(array) {
+    for(let item of array) {
+        if(item.result === "W") {
+            return item.year;
+        }
+    }
+}
+
+// const sadReality = array.find(element => element === "W");
+
+// function superbowlWin(record) {
+//     if (record = sadReality) {
+//         return record.year;
+//     } else return undefined;
+// }
+
+// function superbowlWin(win) {
+// let year = win.year;
+// if (win.result === "W") {
+//     return year;
+// } else {
+//     return undefined;
+// }
+// }
+
+// const finder = record.find(superbowlWin);
+
+
+
